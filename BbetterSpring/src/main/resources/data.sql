8cbd12248247f572d055aab659f7382c9acd61a2
jesusnoseq <jesusnoseq@hotmail.com>
2017-10-20 16:12:17 +0200

    data examples

--- BbetterSpring/src/main/resources/data.sql
+++ BbetterSpring/src/main/resources/data.sql
@@ -1,6 +1,5 @@
-/*
-insert into purpose(id, completed, purpose_date, description) values(1, true,{ts '2017-10-16 00:00:00.00'},'Descripcion de proposito1');
-insert into purpose(id, completed, purpose_date, description) values(2, false,{ts '2017-10-16 00:00:00.00'},'Descripcion de proposito2');
-insert into purpose(id, completed, purpose_date, description) values(3, false,{ts '2017-10-17 00:00:00.00'},'Descripcion de proposito3');
-insert into purpose(id, completed, purpose_date, description) values(4, false,{ts '2017-10-17 00:00:00.00'},'Descripcion de proposito4');
-*/
+
+insert into purpose(id, completed, purpose_date, description) values(1, true,{ts '2017-10-20 00:00:00.00'},'Descripcion de proposito1');
+insert into purpose(id, completed, purpose_date, description) values(2, false,{ts '2017-10-20 00:00:00.00'},'Descripcion de proposito2');
+insert into purpose(id, completed, purpose_date, description) values(3, false,{ts '2017-10-21 00:00:00.00'},'Descripcion de proposito3');
+insert into purpose(id, completed, purpose_date, description) values(4, false,{ts '2017-10-21 00:00:00.00'},'Descripcion de proposito4');
