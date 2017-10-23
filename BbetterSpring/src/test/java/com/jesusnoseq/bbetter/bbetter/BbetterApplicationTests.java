a5605e22ae4130f039d58cfc5e280348251afcac
jesusnoseq <jesusnoseq@hotmail.com>
2017-10-23 13:37:31 +0200

    CORS TEST

--- BbetterSpring/src/test/java/com/jesusnoseq/bbetter/bbetter/BbetterApplicationTests.java
+++ BbetterSpring/src/test/java/com/jesusnoseq/bbetter/bbetter/BbetterApplicationTests.java
@@ -2,15 +2,15 @@ package com.jesusnoseq.bbetter.bbetter;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BbetterApplicationTests {
-
-	@Test
-	public void contextLoads() {
-	}
-
 }
--- BbetterSpring/src/test/java/com/jesusnoseq/bbetter/bbetter/unit/PurposesControllerTest.java
+++ BbetterSpring/src/test/java/com/jesusnoseq/bbetter/bbetter/unit/PurposesControllerTest.java
@@ -18,11 +18,13 @@ import com.jesusnoseq.bbetter.bbetter.purposes.Purpose;
 import com.jesusnoseq.bbetter.bbetter.purposes.PurposesController;
 
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 import static org.hamcrest.core.Is.is;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(PurposesController.class)
@@ -37,20 +39,29 @@ public class PurposesControllerTest {
 	@Test
 	public void getAllPurposesTest() throws Exception{
 		Purpose purpose= new Purpose(0l,LocalDate.now(),"TEST",false);
-		
 		List<Purpose> allPurposes=Collections.singletonList(purpose);
 		
 		given(purposesController.retreiveAllPurposes()).willReturn(allPurposes);
 		
-		
 		MockHttpServletRequestBuilder request = get("/api/purposes/all").contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON);
 		
-	
 		mvc.perform(request)
 			.andExpect(status().isOk())
 			.andExpect(jsonPath("$", hasSize(1)))
-			.andExpect(jsonPath("$[0].id",is(purpose.getId())))
 			.andExpect(jsonPath("$[0].description",is(purpose.getDescription())));
-		
+	    verify(purposesController, times(1)).retreiveAllPurposes();
+	    verifyNoMoreInteractions(purposesController);
 	}
+	
+
+
+
+    @Test
+    public void testCORS() throws Exception {
+    	mvc.perform(get("/api/purposes/all"))
+                .andExpect(header().string("Access-Control-Allow-Origin", "*"))
+                .andExpect(header().string("Access-Control-Allow-Methods", "GET, POST, OPTIONS, DELETE"))
+                .andExpect(header().string("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me"))
+                .andExpect(header().string("Access-Control-Max-Age", "3600"));
+    }
 }
