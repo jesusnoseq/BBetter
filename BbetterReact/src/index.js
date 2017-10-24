e852086ed9f5680ed5ed6d0085634bf384b71471
jesusnoseq <jesusnoseq@hotmail.com>
2017-10-24 18:20:23 +0200

    New header

--- BbetterReact/src/index.js
+++ BbetterReact/src/index.js
@@ -4,17 +4,23 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import promise from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import ThreeDaysView from './components/three_days_view';
-//import Header from './components/Header';
+import Header from './components/Header';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
+const store = createStore(reducers, composeWithDevTools(
+  applyMiddleware(promise, ReduxThunk)
+));
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     <BrowserRouter>
       <div>
+        <Header />
         <Switch>
           <Route exact path="/" component={ThreeDaysView} />
           <Route path="/:year/:month/:day" component={ThreeDaysView} />
