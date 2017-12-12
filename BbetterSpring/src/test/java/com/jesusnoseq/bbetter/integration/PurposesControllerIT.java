8767558674719a24efee925688b502dfd748fdb5
Jesús Rodríguez Pérez <jesusnoseq@hotmail.com>
2017-12-12 15:45:41 +0100

    Unit testing files

---
+++ BbetterSpring/src/test/java/com/jesusnoseq/bbetter/integration/PurposesControllerIT.java
@@ -0,0 +1,95 @@
+package com.jesusnoseq.bbetter.integration;
+
+import java.time.LocalDate;
+import java.util.Collections;
+import java.util.List;
+
+import org.aspectj.lang.annotation.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.http.MediaType;
+
+import com.jesusnoseq.bbetter.bbetter.purposes.Purpose;
+import com.jesusnoseq.bbetter.bbetter.purposes.PurposesController;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.hamcrest.core.Is.is;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(PurposesController.class)
+public class PurposesControllerIT {
+
+	@Autowired
+	private MockMvc mvc;
+	
+	@MockBean
+	private PurposesController purposesController;
+	
+	
+
+	public void prepareMockData(){
+		Purpose purpose= new Purpose(0l,LocalDate.now(),"TEST",false);
+		List<Purpose> allPurposes=Collections.singletonList(purpose);
+	}
+	
+	
+	@Test
+	public void getAllPurposesTest() throws Exception{
+		Purpose purpose= new Purpose(0l,LocalDate.now(),"TEST",false);
+		List<Purpose> allPurposes=Collections.singletonList(purpose);
+		
+		given(purposesController.retreiveAllPurposes()).willReturn(allPurposes);
+		
+		MockHttpServletRequestBuilder request = get("/api/purposes/all").contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON);
+		
+		mvc.perform(request)
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$", hasSize(1)))
+			.andExpect(jsonPath("$[0].description",is(purpose.getDescription())));
+		
+	    verify(purposesController, times(1)).retreiveAllPurposes();
+	    verifyNoMoreInteractions(purposesController);
+	}
+	
+	@Test
+	public void getIdPurposesTest() throws Exception{
+		Purpose purpose= new Purpose(1l,LocalDate.now(),"TEST",false);
+		List<Purpose> allPurposes=Collections.singletonList(purpose);
+		
+		given(purposesController.retreiveAllPurposes()).willReturn(allPurposes);
+		
+		MockHttpServletRequestBuilder request = get("/api/purposes/1").contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON);
+		
+		mvc.perform(request)
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$", hasSize(1)))
+			.andExpect(jsonPath("$[0].description",is(purpose.getDescription())));
+		
+	    verify(purposesController, times(1)).retreiveAllPurposes();
+	    verifyNoMoreInteractions(purposesController);
+
+	}
+	
+
+
+    @Test
+    public void testCORS() throws Exception {
+    	mvc.perform(get("/api/purposes/all"))
+                .andExpect(header().string("Access-Control-Allow-Origin", "*"))
+                .andExpect(header().string("Access-Control-Allow-Methods", "GET, POST, OPTIONS, DELETE, PUT"))
+                .andExpect(header().string("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me"))
+                .andExpect(header().string("Access-Control-Max-Age", "3600"));
+    }
+}
---
+++ BbetterSpring/src/test/java/com/jesusnoseq/bbetter/unit/PurposesRepositoryTest.java
@@ -0,0 +1,19 @@
+package com.jesusnoseq.bbetter.unit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PurposesRepositoryTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
